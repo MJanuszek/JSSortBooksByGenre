1a340cf8655138dc575567bc54031e4dc049654a
MJanuszek <monika.januszek.mail@gmail.com>
2022-07-20 14:20:33 +0200

    Function displayBooks ()
    Selecting genre and displaying results: working functon displayBooks ()

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const buttons = document.querySelectorAll(".genre");
 const displayBooksDiv = document.querySelector(".display-books");
-
+// displayBooksDiv.textContent = "ok"
 
 const allBooks = [
     {
@@ -21,40 +21,61 @@ const allBooks = [
         genre: "romance"
     }
 ];
-
-// extracting "romance" genre from array of objects:
 const romanceBooks = [];
+const crimeBooks = [];
+const horrorBooks = [];
+const adventureBooks = [];
+const fantasyBooks = [];
+// extracting genre from array of objects:
 const extractGenre = (array) => {
     for (let i =0; i <array.length; i++ ){
         if (array[i].genre === "romance") {
             romanceBooks.push(array[i].name)
+        } else if (array[i].genre === "crime") {
+            crimeBooks.push(array[i].name)
         }
     }
-
-}
-
-extractGenre(allBooks)
-
+    //  end of extractGenre()
+};
 
 const displayBooks = (e) => {
-    console.log(e.srcElement);
-    
-    if (e.srcElement.className === "crime") {
-        displayBooksDiv.textContent = "ok";
-    } else if (e.srcElement.className === "romance") {
-        extractGenre(allBooks)
+    console.log(e.srcElement.className);
+        // 1
+    if (e.srcElement.className.includes("crime")) {
+        displayBooksDiv.textContent = crimeBooks;
+        if ( crimeBooks.length === 0){
+            displayBooksDiv.textContent = `Selected ${e.srcElement.className} doesn't exist`;
+        }
+        // 2
+    } else if (e.srcElement.className.includes("romance")) {
         displayBooksDiv.textContent = romanceBooks;
+        if ( romanceBooks.length === 0){
+            displayBooksDiv.textContent = `Selected ${e.srcElement.className} doesn't exist`;
+        }
+        // 3
+    } else if (e.srcElement.className.includes("horror")) {
+        displayBooksDiv.textContent = horrorBooks;
+        if ( horrorBooks.length === 0){
+            displayBooksDiv.textContent = `Selected ${e.srcElement.className} doesn't exist`;
+        }
+        // 4
+    } else if (e.srcElement.className.includes("adventure")) {
+        displayBooksDiv.textContent = adventureBooks;
+        if ( adventureBooks.length === 0){
+            displayBooksDiv.textContent = `Selected ${e.srcElement.className} doesn't exist`;
+        }
+        // 5
+    } else if (e.srcElement.className.includes("fantasy")) {
+        displayBooksDiv.textContent = fantasyBooks;
+        if ( fantasyBooks.length === 0){
+            displayBooksDiv.textContent = `Selected ${e.srcElement.className} doesn't exist`;
+        }
+        
     }
- 
-
-
-
-
-
 
 
 
     // end displayBooks()
 }
-
+extractGenre(allBooks);
 buttons.forEach(button => button.addEventListener("click", displayBooks));
